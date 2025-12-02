72442f883f47d79ec2533ebe5bf44eab08b4bdc4
JosephField09 <JosephField09@hotmail.com>
2025-12-02 02:50:10 +0000

    Day 1 - Actually Complete

--- day_1/Day1Solution.py
+++ day_1/Day1Solution.py
@@ -10,36 +10,40 @@ def get_rotation(line):
         rotation = rotation + l
     return int(rotation)
 
-def rotate(sign, rotation):
+def rotate(sign, rotation, pointing_at, password):
     """
     Rotates the dial by the input rotation.
-    Makes sure the dial doesn't go beyond 99 or below 0.
 
     :param sign: Either + or -
     :param rotation: The amount to rotate by
+    :param pointing_at: Current number it's pointing att
+    :param password: Current total of clicks to 0
     :return: What the dial is pointing at now
     """
-    final = eval(str(pointing_at)+sign+str(rotation))
-    while final > 99 or final < 0:
-        if final > 99:
-            diff = final - 99
-            final = -1 + diff
-        elif final < 0:
-            diff = abs(final)
-            final = 100 - diff
-    return final
+    passed_0 = 0
+    final = pointing_at
+    for i in range(rotation):
+        if sign == "+":
+            final += 1
+            if final > 99:
+                final = 0
+        else:
+            final -= 1
+            if final < 0:
+                final = 99
+        if final == 0:
+            passed_0 += 1
+    password += passed_0
+    return final, password
 
 if __name__=="__main__":
     file = open("day_1_input.txt") # Open file, split lines
     lines = file.read().split()
     pointing_at = 50  # Where the dial starts at
     password = 0
-
     for line in lines:
         sign = "+" if line[0] == "R" else "-"
-        rotatation = get_rotation(line)
-        pointing_at = rotate(sign, rotatation)
-        if pointing_at == 0:
-            password += 1
+        rotation = get_rotation(line)
+        pointing_at, password = rotate(sign, rotation, pointing_at, password)
 
     print(f"Password: {password}")
