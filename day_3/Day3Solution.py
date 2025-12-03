817f96c211c0169e2ea5d24a83389e9a745216f9
JosephField09 <JosephField09@hotmail.com>
2025-12-03 15:30:08 +0000

    Day 3 - Solved (Part 1 ✅, Part 2✅)

--- day_3/Day3Solution.py
+++ day_3/Day3Solution.py
@@ -1,16 +1,19 @@
-def get_joltage(line, total):
-    batteries = list(line)
-    b1 = max(batteries[:len(batteries)-1])
-    i = batteries.index(b1)
-    for l in batteries[:i+1]:
-        batteries.remove(l)
-    b2 = max(batteries)
-    return total + int(b1+b2)
+def get_joltage(line, size):
+    joltage = ""
+    current = 0
+    while len(joltage) < size:
+        next = max(line[current:(len(line)-(size-len(joltage)))+1])
+        joltage += next
+        current = line.index(next, current)+1
+    return int(joltage)
 
 if __name__=="__main__":
-    file = open("day_3_input.txt") # Open file, split lines
+    file = open("day_3_input.txt","r") # Open file, split lines
     lines = file.read().splitlines()
-    total = 0
+    part1 = 0
+    part2 = 0
     for line in lines:
-        total = get_joltage(line, total)
-    print(total)
+        part1 += get_joltage(list(line), 2)
+        part2 += get_joltage(list(line), 12)
+    print(f"Part 1: {part1}")
+    print(f"Part 2: {part2}")
